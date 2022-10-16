6d3272a769770a5aef0b08e5d46c24c89b2ef4de
Panagiotis Panagiotakopoulos <ppanagiotisilv@gmail.com>
2022-10-16 13:11:11 +0300

    new project

---
+++ newbie/product-preview-card-component/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Fraunces', 'serif'],
+    },
+    extend: {
+      colors: {
+        cream: '#F2EBE3',
+        green: '#3C8067',
+        darkBlue: '#1C232B',
+        gray: '#6C7289',
+      }
+    },
+  },
+  plugins: [],
+}
